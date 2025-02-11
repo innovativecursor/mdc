17d49ccec4f455b253d4a34de9b090645a770e11
vaish567 <vaishnav.parte@gmail.com>
2025-02-11 22:03:06 +0530

    update

--- src/app/(frontend)/home/Home.tsx
+++ src/app/(frontend)/home/Home.tsx
@@ -8,9 +8,6 @@ const Home = () => {
       <Header />
 
       {/* Main Content */}
-      <main className="min-h-screen bg-gray-100 flex items-center justify-center">
-        <h1 className="text-3xl font-semibold text-gray-800">Welcome to MDC</h1>
-      </main>
     </div>
   )
 }
